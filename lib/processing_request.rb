f7a5300cb249cea6ab36445540a866484451d585
Владимир Алёхин <v.alekhin@dcrt.it>
2022-02-01 18:12:48 +0300

    Рефакторинг логики и замечаний рубокопа, с комментариями

--- lib/processing_request.rb
+++ lib/processing_request.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 class ProcessingRequest
   require 'faraday'
@@ -6,23 +7,28 @@ class ProcessingRequest
   require 'digest'
   require 'constants'
   require 'dotenv/load'
-  
+
   PUBLIC_KEY = ENV['PUBLIC_KEY']
   PRIVATE_KEY = ENV['PRIVATE_KEY']
+
+  class ResponseDataError < StandardError; end
+  class CharacterNotFound < StandardError; end
+  class ConnectionError < StandardError; end
 
   def initialize(names)
     @names = names
   end
-  
+
   def self.call(...)
     new(...).call
   end
 
   def call
-    @names.map{ |name| process_request(make_url(name), name) }
+    @names.map { |name| process_request(make_url(name), name) }
   end
 
   private
+
   def make_url(name)
     ts = Time.now.to_i
     hash = Digest::MD5.hexdigest(ts.to_s + PRIVATE_KEY + PUBLIC_KEY)
@@ -30,20 +36,24 @@ class ProcessingRequest
   end
 
   def process_request(url, character)
-    begin
-      response = Faraday.get(url)
-    rescue
-      raise StandardError, 'Sorry, connection to Marvel database failed', []
-    else
-      process_data(response.body, character)
-    end
+    # также без begin/end
+    response = Faraday.get(url)
+    process_data(response.body, character)
+    # ловим конкретно ошибку Faraday
+  rescue Faraday::Error => e
+    # рейзим кастомные ошибки, и только их потом перехватываем.
+    raise ConnectionError, "Sorry, connection to Marvel database failed, reason: #{e.message}"
   end
 
   def process_data(data, character)
-    result = JSON.parse(data, symbolize_names: true)[:data].fetch(:results)
-    raise StandardError, "Sorry. Character <<#{character}>> is not found! Choose another", [] if result.empty?
+    parsed_data = JSON.parse(data, symbolize_names: true)[:data]
+    if parsed_data.nil?
+      raise ResponseDataError, "Sorry, there is no data in response from Marvel db. #{data} is returned"
+    end
+
+    result = parsed_data.fetch(:results)
+    raise CharacterNotFound, "Sorry. Character <<#{character}>> is not found! Choose another", [] if result.empty?
 
     result.first.slice(:name, :description)
   end
-
 end
